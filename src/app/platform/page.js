d682483ba3912d0542c04b039001786b1331a59b
Harsh Maheshwari <hmaheshwari@asite.com>
2024-08-10 22:17:17 +0530

    Build Issue Solved

--- src/app/platform/page.js
+++ src/app/platform/page.js
@@ -1,10 +1,13 @@
-import MapComponent from "@/components/common/MapComponent";
+import dynamic from "next/dynamic";
+const MapComponent = dynamic(() => import("@/components/common/MapComponent"), {
+  ssr: false,
+});
 import Image from "next/image";
 
 export default function Page() {
   return (
     <>
-    <MapComponent/>
+      <MapComponent />
     </>
   );
 }
